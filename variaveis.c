24423f735e4fb9b77ce9cb9076e74bdb7e46d4c1
ingridbonifacio <117772818+ingridbonifacio@users.noreply.github.com>
2023-08-15 20:51:07 -0300

    Add files via upload

---
+++ variaveis.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+main(){
+
+    //primeiro passo - declaração de variaveis 
+    char nome[50];
+    int idade;
+    char matricula[15];
+    char endereco[40];
+    char curso[50];
+    int periodo;
+    char disciplinas[100];
+    float mensalidade;
+
+    //segundo passo - entrada de dados
+    printf("Nome: \n");
+    fgets(nome,50 stdin);
+
+    printf("Idade: \n");
+    scanf("%d", &idade);
+    fflush(stdin)
+
+    printf("Matricula: \n");
+    fgets(matricula, 15, stdin);
+
+    printf("Endereco: \n");
+    fgets(endereco, 40, stdin);
+
+    printf("Curso: \n");
+    fgets(curso, 50, stdin);
+
+    printf("Periodo: \n");
+    scanf("%d", &periodo);
+    fflush(stdin);
+
+    printf("Disciplina: \n");
+    fgets(disciplinas, 100, stdin);
+
+    printf("Mensalidade: \n");
+    scanf("%f", &mensalidade);
+
+
+}
